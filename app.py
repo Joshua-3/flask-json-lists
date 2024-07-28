20a485c3531b7a34e7800377078dbeae3632b648
Slakoth <69316271+Joshua-3@users.noreply.github.com>
2024-07-27 21:20:28 -0500

    fixed the new site

--- app.py
+++ app.py
@@ -8,25 +8,29 @@ JSON_FOLDER = 'json_files'
 
 @app.route('/')
 def index():
-    elements = []
-    
-    for filename in os.listdir(JSON_FOLDER):
-        if filename.endswith('.json'):
-            file_path = os.path.join(JSON_FOLDER, filename)
-            
-            with open(file_path, 'r') as file:
-                data = json.load(file)
-            
-            element = {
-                'image': data.get('image', ''),
-                'heading': data.get('heading', ''),
-                'description': data.get('description', ''),  # New field
-                'link': data.get('link', '')
-            }
-            
-            elements.append(element)
-    
-    return render_template('index.html', elements=elements)
+    try:
+        elements = []
+
+        for filename in os.listdir(JSON_FOLDER):
+            if filename.endswith('.json'):
+                file_path = os.path.join(JSON_FOLDER, filename)
+
+                with open('./'+ file_path, 'r') as file:
+                    data = json.load(file)
+
+                element = {
+                    'image': data.get('image', ''),
+                    'heading': data.get('heading', ''),
+                    'description': data.get('description', ''),  # New field
+                    'link': data.get('link', '')
+                }
+                
+
+                elements.append(element)
+
+        return render_template('index.html', elements=elements)
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
     app.run(debug=True)
