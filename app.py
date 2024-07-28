8e9c1cce4df37a946387383a3b5c1e59bdec6c36
Sneethan <53156475+Sneethan@users.noreply.github.com>
2024-07-28 10:52:26 +1000

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+import os
+import json
+
+app = Flask(__name__)
+
+JSON_FOLDER = 'json_files'
+
+@app.route('/')
+def index():
+    elements = []
+    
+    for filename in os.listdir(JSON_FOLDER):
+        if filename.endswith('.json'):
+            file_path = os.path.join(JSON_FOLDER, filename)
+            
+            with open(file_path, 'r') as file:
+                data = json.load(file)
+            
+            element = {
+                'image': data.get('image', ''),
+                'heading': data.get('heading', ''),
+                'description': data.get('description', ''),  # New field
+                'link': data.get('link', '')
+            }
+            
+            elements.append(element)
+    
+    return render_template('index.html', elements=elements)
+
+if __name__ == '__main__':
+    app.run(debug=True)
